6e01b8d2c64e5f7743fa4b19d63ddaf591be7a9a
Marc Poppy <poppy.marc@gmail.com>
2019-06-09 23:57:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,33 @@ class Song
     self.class.all << self
   end
 
+  def self.create #instantiates and saves the song, returns the new song
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name) #instantiates with a song with a name
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{ |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) #if found, return the song
+      song
+    else
+      self.create_by_name(name)
+    end
+  end
 end
