aed93428708af228a88b13fa17e8803a70348d8d
Marc Poppy <poppy.marc@gmail.com>
2019-06-10 00:19:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,6 +47,8 @@ class Song
   def self.new_from_filename(name) #split the name on the -, remove the mp3
     new_name = name.slice! "mp3"
     split_name = new_name.split('-')
+    artist_name = split_name[0].to_s
+    last_name = split_name[1].to_s
   end
 
 
