4249e2107b40087bd24dc191a8b3151bd668ec7e
Marc Poppy <poppy.marc@gmail.com>
2019-06-10 00:21:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ class Song
     split_name = new_name.split('-')
     artist_name = split_name[0].to_s
     song_name = split_name[1].to_s
-    self.create_by_name(song_name)
+    song = self.create_by_name(song_name)
   end
 
 
